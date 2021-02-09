562ae97eca4446ea9ab2241bb5f7aeadd0cace3e
Ibrahim224466 <mercanibrahim@hotmail.com>
2021-02-09 16:14:53 +0100

    Ibrahim first commit

--- src/Practice/Deneme.java
+++ src/Practice/Deneme.java
@@ -2,7 +2,9 @@ package Practice;
 
 public class Deneme {
     public static void main(String[] args) {
+
         System.out.println("Mustafa'dan ilk mesaj");
+        System.out.println("Ibrahim'den ilk mesaj");
 
     }
 
